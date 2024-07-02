e9370312d3c35c254ac55f1e9ceaf716c955f374
PyeongGang-Kim <lefthand1029@gmail.com>
2024-07-02 14:28:51 +0900

    Create netcat.py
    24.07.02

---
+++ Code/Black Hat Python/netcat.py
@@ -0,0 +1,28 @@
+# 넷켓 대체하기
+
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+#전역변수 정의 
+listen = False
+command = False
+upload = False
+execute = " "
+target = " "
+upload_destination = " "
+port = 0
+
+def usage():
+    print("BHP Net Tool")
+    print
+    print("Usage: bhpnet.py -t target_host -p port")
+    print("-l --listen - listen on [host] : [port] for incoming connections")
+    print("-e --execute = file_to_run - execute the given file upon receiving a connection")
+    print("-c --command - initialize a command shell")
+    print("-u --upload=destination - upon receiving connection upload a file and write to [destination]")
+    print
+    print
+    print("Example: ")
